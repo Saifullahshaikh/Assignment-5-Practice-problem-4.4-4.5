fae1171f13adf5b0343119ec7d314ee9e139470a
Saifullahshaikh <ssaifullah272@gmail.com>
2018-11-28 20:58:06 +0500

    Add files via upload

---
+++ P.P 4.5.py
@@ -0,0 +1,12 @@
+print('Saifullah, 18B-092-CS, A')
+print('Assignment# 5, practice problem 4.5')
+
+first = 'Saifullah'
+last = 'Shaikh'
+street = 'PIB Street '
+number = 46
+city = 'Karachi'
+state = 'Pakistan'
+zip_code = '12564'
+mailing_label = ('{} {}\n{} {}\n{}, {} {}')
+print(mailing_label.format(first,last,number,street,city,state,zip_code))
---
+++ P.P4.4.py
@@ -0,0 +1,6 @@
+print('Saifullah, 18B-092-CS, A')
+print('Assignment# 5, practice problem 4.4')
+def even(n):
+      for i in range(n):
+          if i%2 == 0 or i%3 == 0:
+              print(i,end=',')
